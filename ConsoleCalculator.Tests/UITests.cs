31a68915840c0da9a103fbf0e453b2015c95629f
grafsnikers12 <47172346+grafsnikers12@users.noreply.github.com>
2020-12-26 21:41:04 +0500

    Добавлен тест проверяющий, что класс UI завершит работу если получит пустую строку

--- ConsoleCalculator.Tests/UITests.cs
+++ ConsoleCalculator.Tests/UITests.cs
@@ -77,5 +77,32 @@ namespace ConsoleCalculator.Tests
                 Assert.Contains("Goodbye", stringOut.ToString());
             });
         }
+
+        [Fact(Timeout = 1000)]
+        public async Task CloseProgramIfGetEmptyString()
+        {
+            await Task.Run(() =>
+            {
+                string expressions =
+                   "\n";
+                StringReader stringIn = new StringReader(expressions);
+
+                StringWriter stringOut = new StringWriter();
+                StringWriter stringOutErr = new StringWriter();
+
+                var mock = new Mock<ISolver>();
+                mock
+                    .Setup(m => m.Solve(It.IsAny<string>()))
+                    .Throws(new InvalidSyntaxException());
+
+                UI ui = new UI(stringOut, stringIn, stringOutErr);
+
+                // Act
+                ui.Run(mock.Object);
+
+                // Assert
+                Assert.Contains("Goodbye", stringOut.ToString());
+            });
+        }
     }
 }
