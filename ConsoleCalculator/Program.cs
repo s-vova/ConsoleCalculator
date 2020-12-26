48817dc33b6f8b7cd78dcdf11ac76f057c43e084
grafsnikers12 <47172346+grafsnikers12@users.noreply.github.com>
2020-12-26 21:53:30 +0500

    Из метода Main удалена логика взаимодействия с интерфейсом, теперь он вызывает UI

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -9,54 +9,11 @@ namespace ConsoleCalculator
     {
         static void Main()
         {
-            // Словарь, в котором задается: оператор, функция которую он выполняет, и его приоритет
-            Dictionary<string, Operator> OperatorsDict = new Dictionary<string, Operator>
-            {
-                { "+", new Operator((a,b) => a+b, priority : 1) },
-                { "-", new Operator((a,b) => a-b, priority : 1) },
-                { "*", new Operator((a,b) => a*b, priority : 2) },
-                { "/", new Operator((a,b) => b==0 ? throw new DivideByZeroException() : a/b, priority : 2) },
-                // Пример добавления новго оператора, обратите внимание, что приоритет возведения в степень выше, чем у * и /
-                // { "^", new Operator((a,b) => Math.Pow(a,b), priority : 3) }, 
-                // Пример добавления нового оператора
-                // { "%", new Operator((a,b) => b==0 ? throw new DivideByZeroException() : a%b, priority : 2) },  
-            };
-
-            Console.WriteLine("Welcome to Calculator");
-            Console.WriteLine($"Allowed operators: {string.Join(" ", OperatorsDict.Keys)}");
-            Console.WriteLine("To close the program, enter empty string");
-            Console.WriteLine();
-
             Calculator calculator = new Calculator();
+            Solver solver = new Solver(calculator, Parser.Parse);
+            UI ui = new UI(Console.Out, Console.In, Console.Error);
 
-            while (true)
-            {
-                Console.Write("Enter expression: ");
-                string expression = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(expression))
-                {
-                    break;
-                }
-
-                try
-                {
-                    IList<Token> tokensList = Parser.Parse(expression, OperatorsDict.Keys);
-
-                    double result = calculator.Compute(tokensList, OperatorsDict);
-                    Console.WriteLine($"Result: {result}");
-                }
-                catch (DivideByZeroException)
-                {
-                    Console.WriteLine("Zero division error occured!");
-                }
-                catch (InvalidSyntaxException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-
-            }
-            Console.WriteLine("Goodbye!");
+            ui.Run(solver);
         }
     }
 }
