9e08589ecd893611cedb1bf269d9dbb085eff930
grafsnikers12 <47172346+grafsnikers12@users.noreply.github.com>
2020-12-26 21:26:16 +0500

    Добавлен класс UI

---
+++ ConsoleCalculator.Tests/UITests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using System.IO;
+using ConsoleCalculator.Interfaces;
+using ConsoleCalculator;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator.Tests
+{
+    public class UITests
+    {
+        // Этот тест сделан асинхронным, потому что в xUnit есть баг, 
+        // из-за которого Timeout работает только если метод асинхронный 
+        // ссылка https://github.com/xunit/xunit/issues/217#issuecomment-625402054
+        // таймаут нужен потому что есть вероятность попадания в бесконечный цикл
+        [Fact(Timeout = 1000)]
+        public async Task UIPrintsResultFromSolver()
+        {
+            await Task.Run(() =>
+            {
+                string expressions =
+                   "1 + 1\n" +
+                   "2 + 2\n" +
+                   "\n";
+                StringReader stringIn = new StringReader(expressions);
+
+                StringWriter stringOut = new StringWriter();
+                StringWriter stringErr = new StringWriter();
+
+                var mock = new Mock<ISolver>();
+                mock
+                    .SetupSequence(m => m.Solve(It.IsAny<string>()))
+                    .Returns(2)
+                    .Returns(4)
+                    .Throws(new InvalidOperationException());
+
+                UI ui = new UI(stringOut, stringIn, stringErr);
+
+                // Act
+                ui.Run(mock.Object);
+
+                // Assert
+                Assert.Contains("Result: 2", stringOut.ToString());
+                Assert.Contains("Result: 4", stringOut.ToString());
+            });
+           
+        }
+    }
+}
---
+++ ConsoleCalculator/UI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ConsoleCalculator.Interfaces;
+using System.Threading;
+
+namespace ConsoleCalculator
+{
+    public class UI
+    {
+        private readonly TextReader Input;
+        private readonly TextWriter Out;
+        private readonly TextWriter OutErr;
+
+        public UI( TextWriter Out, TextReader Input, TextWriter OutErr)
+        {
+            this.Input = Input;
+            this.Out = Out;
+            this.OutErr = OutErr;
+        }
+
+        public void Run(ISolver solver)
+        {
+            Out.WriteLine("Welcome to Calculator");
+            Out.WriteLine("To close the program, enter empty string");
+            Out.WriteLine();
+
+            while (true)
+            {
+                Out.Write("Enter expression: ");
+                string expression = Input.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(expression))
+                {
+                    break;
+                }
+
+                double result = solver.Solve(expression);
+                Out.WriteLine($"Result: {result}");
+            }
+            Out.WriteLine("Goodbye!");
+        }
+    }
+}
+
